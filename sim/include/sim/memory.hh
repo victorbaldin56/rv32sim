2014bb3af5bc387d62a176ec20b8bdbeedd5bccf
Victor Baldin <110900022+victorbaldin56@users.noreply.github.com>
2025-10-31 18:09:05 +0300

    Use memory mapping for memory model instead of std::vector to increase performance (#3)

--- sim/include/sim/memory.hh
+++ sim/include/sim/memory.hh
@@ -18,37 +18,43 @@
 #include <type_traits>
 #include <vector>
 
+#include "base/mmaped_file.hh"
 #include "types.hh"
 
 namespace rv32 {
 
 class Memory final {
  public:
-  Memory() : mem_(kAddressSpaceSize) {}
+  Memory()
+      : mem_(makeUniqueMmap(nullptr, kAddressSpaceSize, PROT_WRITE,
+                            MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) {}
+
+  Size size() const noexcept { return mem_.get_deleter().len; }
+
   void copy(Addr dst, const void* src, Size sz) noexcept {
-    assert(dst + sz <= mem_.size());
-    std::memcpy(&mem_[dst], src, sz);
+    assert(dst + sz <= size());
+    std::memcpy(&operator[](dst), src, sz);
   }
 
   void copy(void* dst, Addr src, Size sz) noexcept {
-    assert(src + sz <= mem_.size());
-    std::memcpy(dst, &mem_[src], sz);
+    assert(src + sz <= size());
+    std::memcpy(dst, &operator[](src), sz);
   }
 
   void memset(Addr p, Byte byte, Size sz) noexcept {
-    assert(p + sz <= mem_.size());
-    std::memset(&mem_[p], byte, sz);
+    assert(p + sz <= size());
+    std::memset(&operator[](p), byte, sz);
   }
 
   template <typename T>
   void emit(Addr p, T value) noexcept {
-    static_assert(std::is_standard_layout_v<T>);
+    static_assert(std::is_trivially_copyable_v<T>);
     copy(p, std::addressof(value), sizeof(value));
   }
 
   template <typename T>
   T get(Addr p) {
-    static_assert(std::is_standard_layout_v<T>);
+    static_assert(std::is_trivially_copyable_v<T>);
     T value;
     copy(std::addressof(value), p, sizeof(value));
     return value;
@@ -56,20 +62,22 @@ class Memory final {
 
   // TODO: error handling
   SSize read(int fd, Addr buf, Size count) {
-    assert(buf + count <= mem_.size());
-    return ::read(fd, &mem_[buf], count);
+    assert(buf + count <= size());
+    return ::read(fd, &operator[](buf), count);
   }
 
   // TODO: error handling
   SSize write(int fd, Addr buf, Size count) const {
-    assert(buf + count <= mem_.size());
-    return ::write(fd, &mem_[buf], count);
+    assert(buf + count <= size());
+    return ::write(fd, &operator[](buf), count);
   }
 
-  std::size_t size() const noexcept { return mem_.size(); }
-
  private:
-  std::vector<Byte, boost::noinit_adaptor<std::allocator<Byte>>> mem_;
+  UniqueMmap mem_;
+
+  Byte* data() const noexcept { return static_cast<Byte*>(mem_.get()); }
+  Byte& operator[](Size n) { return data()[n]; }
+  const Byte& operator[](Size n) const { return data()[n]; }
 
   static constexpr Size kAddressSpaceSize =
       0x80000000;  // we dont need 4 GB because in absence of kernel space.
